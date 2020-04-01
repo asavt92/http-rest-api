6caa6aa9f1ebf6f5b95d2bdd5841da9dd579830a
asavt92 <asavt92@gmail.com>
2020-04-01 16:59:00 +0400

    basic rest api /hello

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"gihub.com/asavt/http-rest-api/internal/app/apiserver"
+	"github.com/BurntSushi/toml"
+	"log"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
